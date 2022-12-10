47aaa516e2c2b46f73db8c1fa98e36e6d7459abe
Поремский <mr.poremskiy@mail.ru>
2022-12-10 10:42:27 +0300

    Добавьте файлы проекта.

---
+++ Provodnik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using provodnik;
+
+internal class Program
+{
+    public static void Main()
+    {
+        dop.DiskInf();
+        menu.Strelki();
+    }
+}
